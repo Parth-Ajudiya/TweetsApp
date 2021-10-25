f359fe1fd69335a38c762ad8d030eab9191ee92f
parth <parthajudiya96647@gmail.com>
2021-10-25 09:53:01 +0530

    create authenticate system

--- test/controllers/staticpages_controller_test.rb
+++ test/controllers/staticpages_controller_test.rb
@@ -2,29 +2,24 @@ require "test_helper"
 
 class StaticpagesControllerTest < ActionDispatch::IntegrationTest
 
-  def setup
-    @base_title = "Ruby on Rails Tutorial"
-  end
-
   test "should get home" do
-    get staticpages_home_url
+    get root_path
     assert_response :success
-    assert_select 'title', "Home | #{@base_title}"
-
   end
 
   test "should get help" do
-    get staticpages_help_url
+    get help_path
     assert_response :success
-    assert_select 'title', "Help | #{@base_title}"
-
   end
 
   test "should get about" do
-    get staticpages_about_url
+    get about_path
     assert_response :success
-    assert_select 'title', "About | #{@base_title}"
-
+  end
+  
+  test "should get contact" do
+    get contact_path
+    assert_response :success
   end
 
 end
