6a988f005a251fa56a2fcc06c5c52617cd152513
parth <parthajudiya96647@gmail.com>
2021-10-28 09:47:03 +0530

    add remember method

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,15 @@
 Rails.application.routes.draw do
   root 'staticpages#home'
 
-  get '/signup', to: 'users#new'
-  get '/home', to: 'staticpages#home'
-  get '/help', to: 'staticpages#help'
-  get '/about', to: 'staticpages#about'
-  get '/contact', to: 'staticpages#contact'
+  get 'signup', to: 'users#new'
+  get 'home', to: 'staticpages#home'
+  get 'help', to: 'staticpages#help'
+  get 'about', to: 'staticpages#about'
+  get 'contact', to: 'staticpages#contact'
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
+
   resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
